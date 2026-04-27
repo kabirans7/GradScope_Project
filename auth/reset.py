d7404ae1987baec13623ada4b31daee8e73f2f8b
Kabir Ansari <kabir.ansari@outlook.com>
2026-04-27 11:12:32 +0100

    Update reset.py

--- auth/reset.py
+++ auth/reset.py
@@ -1,32 +1,34 @@
-import streamlit as st
-from .auth_state import card_start, card_end
-from backend.auth_repo import reset_password_with_token 
+# Admin Codes - Will be integrated in future iteration 
 
-def render_reset():
-    card_start("Reset Password", "Please enter your new password below")
+# import streamlit as st
+# from .auth_state import card_start, card_end
+# from backend.auth_repo import reset_password_with_token 
 
-    token = st.session_state.get("reset_token", "")
-    if not token:
-        st.error("Reset token missing. Please use the link from your email.")
-        card_end()
-        return
+# def render_reset():
+#     card_start("Reset Password", "Please enter your new password below")
 
-    new_pw = st.text_input("New Password", type="password", key="new_pw")
-    confirm_pw = st.text_input("Confirm New Password", type="password", key="confirm_pw")
+#     token = st.session_state.get("reset_token", "")
+#     if not token:
+#         st.error("Reset token missing. Please use the link from your email.")
+#         card_end()
+#         return
 
-    if st.button("Update Password", key="btn_update_password", use_container_width=True):
-        if not new_pw or not confirm_pw:
-            st.error("Please fill in both password fields.")
-        elif new_pw != confirm_pw:
-            st.error("Passwords do not match.")
-        else:
-            ok, msg = reset_password_with_token(token, new_pw)
-            if ok:
-                st.session_state["auth_view"] = "reset_success"
-                st.session_state.pop("reset_token", None)
-                st.query_params.clear()
-                st.rerun()
-            else:
-                st.error(msg)
+#     new_pw = st.text_input("New Password", type="password", key="new_pw")
+#     confirm_pw = st.text_input("Confirm New Password", type="password", key="confirm_pw")
 
-    card_end()
+#     if st.button("Update Password", key="btn_update_password", use_container_width=True):
+#         if not new_pw or not confirm_pw:
+#             st.error("Please fill in both password fields.")
+#         elif new_pw != confirm_pw:
+#             st.error("Passwords do not match.")
+#         else:
+#             ok, msg = reset_password_with_token(token, new_pw)
+#             if ok:
+#                 st.session_state["auth_view"] = "reset_success"
+#                 st.session_state.pop("reset_token", None)
+#                 st.query_params.clear()
+#                 st.rerun()
+#             else:
+#                 st.error(msg)
+
+#     card_end()
